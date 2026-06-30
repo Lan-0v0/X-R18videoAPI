7a4b32ec34adf55f4060b263aa6fc7256df8fc30
梦于枫岚 <mengyufenglan@126.com>
2026-06-30 18:49:53 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-call "D:\AstrBot\AstrBot-deploy\venv\Scripts\activate.bat"
+call "D:\AstrBot\AstrBot-deploy\venv\Scripts\activate.bat"#路径仅供参考
 pip install flask>=2.0.0 requests>=2.25.0 beautifulsoup4>=4.9.0
 python AstrBot_X-R18videoAPI.py
 pause
